82398071090280aceb3ba5b8ce8abc9dfbd61cd9
cborjah <cpborja32@gmail.com>
2018-10-10 18:49:09 -0700

    adds checkbox and dynamic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Checkbox from "@material-ui/core/Checkbox";
+
 class App extends Component {
+  state = { isChecked: false };
+
+  handleOnChange = () => this.setState({ isChecked: !this.state.isChecked });
+
   render() {
+    const fooStyles = this.state.isChecked ? "hidden" : null;
+    const barStyles = this.state.isChecked ? "" : "hidden";
+
     return (
       <div className="container">
+        <Checkbox
+          color="secondary"
+          onChange={this.handleOnChange}
+        />
         <div id="foo">
-          <h1>FOO</h1>
+          <h1 className={fooStyles}>FOO</h1>
         </div>
         <div id="bar">
-          <h1>BAR</h1>
+          <h1 className={barStyles}>BAR</h1>
         </div>
       </div>
     );
