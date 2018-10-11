e0a3fd437c7cdc4bef0285ab8da6e5f0285e58ac
cborjah <cpborja32@gmail.com>
2018-10-10 17:43:06 -0700

    starts static layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Hello World!
-        </header>
+      <div className="container">
+        <div id="foo">
+          <h1>FOO</h1>
+        </div>
+        <div id="bar">
+          <h1>BAR</h1>
+        </div>
       </div>
     );
   }
